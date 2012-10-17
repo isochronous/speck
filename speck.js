1f09596a2ed4502a9884c679e3922ef4284b3f23
Jeremy McLeod <isochronous@gmail.com>
2012-10-17 10:19:05 -0400

    marionette version
    Added support for marionette's serializeData() method in speck.view(),
    also updated speck.html() to NOT update the contents of the View's $el
    after rendering by default, though you can pass "true" as the third
    parameter and it will update the UI.

--- speck.js
+++ speck.js
@@ -1,66 +1,79 @@
-define(['backbone', 'module', 'text', 'dustjs-linkedin'], function(Backbone, module, text, dust) {
-	var masterConfig = module.config();
-	return {
-		load: function(name, req, onLoad, config) {
-			var url = name;
-			if(masterConfig && 'speckUrl' in masterConfig && url[0] !== '.'){
-				url = masterConfig.speckUrl + url;
-			}
-			if (url.indexOf('.dust', url.length - 5) === -1) {
-				url = url + '.dust';
-			}
-			text.get(req.toUrl(url), function(data) {
-				var compiled = dust.compile(data, name);
-				dust.loadSource(compiled);
-				var normalizeObject = function(obj) {
-						if (obj === undefined || obj === null) {
-							return {};
-						}
-						return JSON.parse(JSON.stringify(obj));
-					};
-				var render = function(element, obj){
-					var ret = Backbone.$.Deferred();
-					dust.render.call(dust, name, obj, function(err, out) {
-						if (err) {
-							ret.reject(err);
-							throw err;
-						}
-						if(!('jquery' in element)) {
-							element = Backbone.$(element);
-						}
-						element.html(out);
-						ret.resolve(out);
-					});
-					return ret;
-				};
-				var out = {
-					render: function(obj, callback) {
-						dust.render.call(dust, name, obj, callback);
-					},
-					html: function(element, obj) {
-						return render(element, obj);
-					},
-					view: function(view, replace){
-						var obj = normalizeObject(view.model);
-						var context = dust.makeBase(view);
-						var instance = context.push(obj);
-						if(replace) {
-							var old = view.$el;
-							return render(view.$el, instance).done(function(){
-								var content = view.$el.contents();
-								//put it in the correct place in the dom:
-								view.$el.replaceWith(content);
-								view.setElement(content);
-							});
-						}
-						return render(view.$el, instance);
-					},
-					url: url,
-					name: name,
-					compiled: compiled
-				};
-				onLoad(out);
-			});
-		}
-	};
+define(['jquery', 'backbone', 'module', 'text', 'dustjs-linkedin'], function($, Backbone, module, text, dust) {
+    var masterConfig = module.config();
+    return {
+        load: function(name, req, onLoad, config) {
+            var url = name;
+            if(masterConfig && 'speckUrl' in masterConfig && url[0] !== '.'){
+                url = masterConfig.speckUrl + url;
+            }
+            if (url.indexOf('.dust', url.length - 5) === -1) {
+                url = url + '.dust';
+            }
+            //TODO: Perhaps hook this up to marionette's template cache?
+            text.get(req.toUrl(url), function(data) {
+                var compiled = dust.compile(data, name);
+                dust.loadSource(compiled);
+                var normalizeObject = function(obj) {
+                    if (obj === undefined || obj === null) {
+                        return {};
+                    }
+                    return JSON.parse(JSON.stringify(obj));
+                };
+
+                // Added updateUI to allow render method to NOT update UI
+                // if we just want to get the HTML back
+                var render = function(element, obj, updateUi){
+                    var ret = $.Deferred();
+
+                    dust.render.call(dust, name, obj, function(err, out) {
+                        if (err) {
+                            ret.reject(err);
+                            throw err;
+                        }
+                        if(!('jquery' in element)) {
+                            element = $(element);
+                        }
+                        if (updateUi === true) {
+                            element.html(out);
+                        }
+                        ret.resolve(out);
+                    });
+                    return ret;
+                };
+                var out = {
+                    render: function(obj, callback) {
+                        dust.render.call(dust, name, obj, callback);
+                    },
+                    // Modified: now takes an optional third param (defaults to false) that determines
+                    // whether or not the call to HTML updates the UI as well as returning the markup
+                    html: function(element, obj, update) {
+                        var updateUi = update || false;
+                        // Modified: added normalization to call
+                        return render(element, normalizeObject(obj), updateUi);
+                    },
+                    view: function(view, replace){
+                        // Marionette views have a serializeData method that determines
+                        // which data should be attached to the view
+                        var obj = normalizeObject(view.serializeData());
+                        var context = dust.makeBase(view);
+                        var instance = context.push(obj);
+                        if(replace) {
+                            var old = view.$el;
+                            return render(old, instance, true).done(function(){
+                                var content = old.contents();
+                                //put it in the correct place in the dom:
+                                old.replaceWith(content);
+                                view.setElement(content);
+                            });
+                        }
+                        return render(view.$el, instance, true);
+                    },
+                    url: url,
+                    name: name,
+                    compiled: compiled
+                };
+                onLoad(out);
+            });
+        }
+    };
 });
